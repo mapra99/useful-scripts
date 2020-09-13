50d63fb76f67475ed4977d2bfdcb5593b8c59bbe
Miguel Prada <mapra99@gmail.com>
2020-09-13 09:51:38 -0500

    webp convert

--- webp_convert.sh
+++ webp_convert.sh
@@ -1,6 +1,16 @@
-for FILE_PATH in $(cat "@")
+#!/bin/bash
+# Converts to webp format all the images found in a specific directory
+# Uses cwebp to convert the images
+# Usage: webp_convert <dir_path>
+# The output images will be saved in the same directory specified by <dir_path>
+
+for FILE in $1*
 do
-	echo "converting ${FILE_PATH}"
-	cwebp ${FILE_PATH} -o ${FILE_PATH}.webp
+  if [[ $FILE =~ \.(jpg|jpeg|svg|gif|png)$ ]]
+  then
+    FILE_WITH_NO_EXT=$(echo "$FILE" | cut -f 1 -d '.')
+    cwebp "${@:2}" "$FILE" -o "$FILE_WITH_NO_EXT.webp"
+  fi
+  # cwebp ${FILE_PATH} -o ${FILE_PATH}.webp
 done
 echo "done :)"
